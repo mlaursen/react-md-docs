7c5172ab20411e13e60b11ca57e3940c0936bf5f
Mikkel Laursen <laursenm@teds.com>
2016-03-21 15:27:03 -0400

    Added a lorem ipsum component

---
+++ src/js/components/LoremIpsum.jsx
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import loremIpsum from 'lorem-ipsum';
+
+export default class LoremIpsum extends Component {
+  constructor(props) {
+    super(props);
+
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  static propTypes = {
+    count: PropTypes.number.isRequired,
+    units: PropTypes.oneOf(['sentences', 'words', 'paragraphs']),
+  };
+
+  static defaultProps = {
+    count: 1,
+    units: 'paragraphs',
+  };
+
+  render() {
+    const { count, units, ...props } = this.props;
+    return (
+      <div
+        {...props}
+        dangerouslySetInnerHTML={{
+          __html: loremIpsum({ count, units, format: 'html' }),
+        }}
+      />
+    );
+  }
+}
