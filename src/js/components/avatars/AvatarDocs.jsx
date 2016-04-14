8f839901573fa039f98871eca2a710b3b7499d39
Mikkel Laursen <mlaursen03@gmail.com>
2016-04-14 18:10:36 -0400

    Added avatar docs (#4)

--- src/js/components/avatars/AvatarDocs.jsx
+++ src/js/components/avatars/AvatarDocs.jsx
@@ -4,6 +4,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import DocPage from 'react-doc-page';
 import AvatarExamples from './AvatarExamples';
 import AvatarExamplesRaw from '!!raw!./AvatarExamples';
+import Avatar from '!!json!docgen/Avatar.json';
 
 export default class AvatarDocs extends Component {
   constructor(props) {
@@ -22,41 +23,7 @@ export default class AvatarDocs extends Component {
           code: AvatarExamplesRaw,
           children: <AvatarExamples />,
         }]}
-        components={[{
-          name: 'Avatar',
-          children: false,
-          props: [{
-            name: 'src',
-            desc: 'The image source for the avatar if you want to use an image.',
-            type: 'string',
-          }, {
-            name: 'alt',
-            desc: 'The image alt tag.',
-            type: 'string',
-          }, {
-            name: 'icon',
-            desc: 'The FontIcon you want to use in the avatar.',
-            type: 'node',
-          }, {
-            name: 'children',
-            desc: 'The letter you want to use in the avatar.',
-            type: 'string',
-          }, {
-            name: 'random',
-            desc: `A boolean that enables adding a random material design color from the available
-            avatar css color classes.`,
-            type: 'bool',
-          }, {
-            name: 'suffix',
-            desc: 'The max color number that can be used when generating a random color for the avatar.',
-            type: 'number',
-          }, {
-            name: 'suffixes',
-            desc: 'A list of available suffixes for random colors.',
-            type: 'arrayOf(string)',
-            defaultValue: `['color-1', 'color-2', 'color-3']`,
-          }],
-        }]}
+        docgens={[Avatar]}
       />
     );
   }
