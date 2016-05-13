a2cf1c827eaac09174bdc3476696eb74028321ee
Mikkel Laursen <laursenm@teds.com>
2016-05-13 12:53:19 -0400

    Updated exisitng stuff to use new PhoneSize component

--- src/js/components/text-fields/FullWidthExamples.jsx
+++ src/js/components/text-fields/FullWidthExamples.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import Divider from 'react-md/lib/Dividers';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import TextField from 'react-md/lib/TextFields';
-import Paper from 'react-md/lib/Papers';
 import { IconButton } from 'react-md/lib/Buttons';
-import Toolbar from 'react-md/lib/Toolbars';
+
+import PhoneSize from '../PhoneSize';
 
 export default class FullWidthExamples extends Component {
   constructor(props) {
@@ -17,41 +17,37 @@ export default class FullWidthExamples extends Component {
     return (
       <div>
         <p>Floating labels are automatically disabled for fullWidth text fields. Use the <code>block</code> prop to enable fullWidth text fields.</p>
-        <Paper className="phone-size-container">
-          <Toolbar
-            secondary
-            title="Compose"
-            actionLeft={<IconButton>arrow_back</IconButton>}
-            actionsRight={<IconButton className="md-toolbar-item justify-end">send</IconButton>}
+        <PhoneSize
+          iconLeft="arrow_back"
+          actionsRight={<IconButton className="md-toolbar-item justify-end">send</IconButton>}
+          contentComponent="form"
+        >
+          <TextField
+            label="From"
+            type="email"
+            block={true}
           />
-          <form>
-            <TextField
-              label="From"
-              type="email"
-              block={true}
-            />
-            <Divider />
-            <TextField
-              label="To"
-              type="email"
-              block={true}
-            />
-            <Divider />
-            <TextField
-              label="Subject"
-              maxLength={80}
-              block={true}
-            />
-            <Divider />
-            <TextField
-              placeholder="Message"
-              rows={2}
-              maxRows={-1}
-              block={true}
-              maxLength={120}
-            />
-          </form>
-        </Paper>
+          <Divider />
+          <TextField
+            label="To"
+            type="email"
+            block={true}
+          />
+          <Divider />
+          <TextField
+            label="Subject"
+            maxLength={80}
+            block={true}
+          />
+          <Divider />
+          <TextField
+            placeholder="Message"
+            rows={2}
+            maxRows={-1}
+            block={true}
+            maxLength={120}
+          />
+        </PhoneSize>
       </div>
     );
   }
--- src/js/components/toolbars/ToolbarWithTabsExample.jsx
+++ src/js/components/toolbars/ToolbarWithTabsExample.jsx
@@ -5,6 +5,7 @@ import Toolbar from 'react-md/lib/Toolbars';
 import { IconButton } from 'react-md/lib/Buttons';
 import { Tabs, Tab } from 'react-md/lib/Tabs';
 import Paper from 'react-md/lib/Papers';
+import LoremIpsum from '../LoremIpsum';
 
 export default class ToolbarWithTabsExample extends Component {
   constructor(props) {
@@ -28,14 +29,15 @@ export default class ToolbarWithTabsExample extends Component {
           )}
         >
           <Tabs primary>
-            {Array.apply(null, new Array(3)).map((_, i) => (
-              <Tab
-                key={i}
-                label={`Item ${i}`}
-              >
-                <div style={{ minHeight: '80px', background: '#fafafa' }} />
-              </Tab>
-            ))}
+            <Tab label="Item 1">
+              <LoremIpsum count={3} />
+            </Tab>
+            <Tab label="Item 2">
+              <LoremIpsum count={3} />
+            </Tab>
+            <Tab label="Item 3">
+              <LoremIpsum count={3} />
+            </Tab>
           </Tabs>
         </Toolbar>
       </Paper>
