d059a8a481d40c47a3bad7a04f902545ce14afe7
Mikkel Laursen <laursenm@teds.com>
2016-05-13 08:52:45 -0400

    Moved proptype string stuff to StringUtils and added shape

--- src/js/components/Docgen/PropType.jsx
+++ src/js/components/Docgen/PropType.jsx
@@ -3,7 +3,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { TableColumn } from 'react-md/lib/DataTables';
 
 import Markdown from '../../containers/Markdown';
-import { tab } from '../../utils/StringUtils';
+import { getPropTypeString } from '../../utils/StringUtils';
 
 export default class PropType extends Component {
   constructor(props) {
@@ -25,62 +25,10 @@ export default class PropType extends Component {
     required: PropTypes.bool,
   };
 
-  handleEnum = (value, computed, tabs) => {
-    let v;
-    if(computed) {
-      v = 'computed';
-    } else {
-      const values = tab(tabs) + value.map(v => v.value).join(',\n' + tab(tabs));
-      v = `[\n${values}\n]`;
-    }
-
-    return `oneOf(${v})`;
-  };
-
-  handleUnion = (value, computed, tabs) => {
-    let v;
-    if(computed) {
-      v = 'computed';
-    } else {
-      v = value.map(v => this.getFullName(v)).join(',\n' + tab(tabs));
-    }
-
-    return `oneOfType([\n${tab(tabs) + v}\n${tab(tabs - 1)}])`;
-  };
-
-  handleComputed = (name, value) => {
-    const computed = eval(value);
-    switch(name) {
-      case 'enum':
-        return this.handleEnum(computed.map(v => ({ value: `'${v}'`})));
-      case 'union':
-        return computed;
-      default:
-        return computed;
-    }
-  };
-
-  getFullName = ({ name, value, computed }, tabs = 0) => {
-    if(computed) {
-      return this.handleComputed(name, value);
-    }
-
-    switch(name) {
-      case 'union':
-        return this.handleUnion(value, computed, tabs + 1);
-      case 'arrayOf':
-        return `${name}(${this.getFullName(value, tabs + 1)})`;
-      case 'enum':
-        return this.handleEnum(value, computed, tabs + 1);
-      default:
-        return name;
-    }
-  };
-
   render() {
     const { type, required } = this.props;
 
-    const markdown = `\`\`\`js\n${this.getFullName(type) + (required ? ' *' : '')}\n\`\`\``;
+    const markdown = `\`\`\`js\n${getPropTypeString(type) + (required ? ' *' : '')}\n\`\`\``;
     return (
       <TableColumn className="prop-name justified" tooltipLabel={required ? 'Required' : null} tooltipPosition="top" tooltipDelay={300}>
         <Markdown markdown={markdown} />
--- src/js/utils/StringUtils.js
+++ src/js/utils/StringUtils.js
@@ -19,3 +19,69 @@ export const toTitle = s => reduce(s, '-', ' ', capitalizeFirst);
 export const toClassName = s => reduce(s, ' ', '-', s => s.toLowerCase());
 export const toPageName = s => reduce(s, '-', '', capitalizeFirst);
 export const tab = (amount = 1) => ' '.repeat(amount);
+
+/*eslint-disable no-use-before-define*/
+
+function getOneOfPropType(value, tabs) {
+  const values = tab(tabs) + value.reduce((prev, curr) => {
+    return prev + curr.value + ',\n' + tab(tabs);
+  }, '');
+
+  return `oneOf([\n${values}\n])`;
+}
+
+function getOneOfTypePropType(value, tabs) {
+  const l = value.length - 1;
+  const values = value.reduce((prev, curr, i) => {
+    const v = prev + getPropTypeString(curr, tabs + 1);
+    return v + (i < l ? ',\n' + tab(tabs) : '');
+  }, '\n' + tab(tabs)) + '\n' + tab(tabs - 1);
+
+  return `oneOfType([${values}])`;
+}
+
+function getShapePropType(value, tabs) {
+  const keys = Object.keys(value);
+  const l = keys.length - 1;
+  const values = keys.reduce((prev, key, i) => {
+    const v = prev + key + ': ' + getPropTypeString(value[key], tabs + 1);
+    return v + (i < l ? ',\n' + tab(tabs) : '');
+  }, '\n' + tab(tabs)) + '\n' + tab(tabs - 1);
+
+  return `shape({${values}})`;
+}
+
+function getComputedPropType(name, value) {
+  const computed = eval(value);
+  switch(name) {
+    case 'enum':
+      return getOneOfPropType(computed.map(v => ({ value: `'${v}'` })));
+    default:
+      return computed;
+  }
+}
+
+/**
+ * Converts a docgen prop type into a string.
+ *
+ * @param {Object} docgen The docgen object.
+ * @param {Number} tabs? The current number of tabs.
+ */
+export function getPropTypeString({ name, value, computed }, tabs = 0) {
+  if(computed) {
+    return getComputedPropType(name, value);
+  }
+
+  switch(name) {
+    case 'union':
+      return getOneOfTypePropType(value, tabs + 1);
+    case 'arrayOf':
+      return `${name}(${getPropTypeString(value, tabs + 1)})`;
+    case 'enum':
+      return getOneOfPropType(value, tabs + 1);
+    case 'shape':
+      return getShapePropType(value, tabs + 1);
+    default:
+      return name;
+  }
+}
